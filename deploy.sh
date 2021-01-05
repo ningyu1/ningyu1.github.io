a0d397a51a87e650e5f6c413638f3be680cb9e5d
ningyu1 <ningbe111@163.com>
2021-01-05 17:46:25 +0800

    测试travis构建失败

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,24 @@
 set -e
 
 # 生成静态文件
-hexo generate
+#hexo generate
+
+if [ ! -d "./public1" ]; then
+  echo "生成失败"!
+  exit 1
+fi
 
 # 进入生成的文件夹
 cd ./public
 
 #创建.nojekyll 防止Github Pages build错误
-touch .nojekyll
+#touch .nojekyll
 
-git init
-git config user.name "ningyu1"
-git config user.email "ningbe111@163.com"
-git add -A .
-git commit -m "Update blog"
-git push -f "https://${access_token}@github.com/ningyu1/blog.git" master:master
+#git init
+#git config user.name "ningyu1"
+#git config user.email "ningbe111@163.com"
+#git add -A .
+#git commit -m "Update blog"
+#git push -f "https://${access_token}@github.com/ningyu1/blog.git" master:master
 
 cd -
